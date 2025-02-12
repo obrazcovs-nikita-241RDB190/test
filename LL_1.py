5fbd28fb2428f9212a7986ebe7d922ac31058a8a
obrazcovs-nikita-241RDB190 <nikita.obrazcovs@edu.rtu.lv>
2025-02-12 11:08:58 +0000

    completed both tasks

--- LL_1.py
+++ LL_1.py
@@ -23,19 +23,36 @@ class LinkedList:
         
 
     # WRITE FIND_MIDDLE_NODE METHOD HERE #
-    #                                    #
-    #                                    #
-    #                                    #
-    #                                    #
+    def find_middle_node(self):
+        slow = self.head
+        fast = self.head
+        while True:
+            if fast == None or fast.next == None:
+                return slow
+            else:
+                fast = fast.next.next
+                slow = slow.next
     ######################################
 
 
 
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
-my_linked_list.append(3)
-my_linked_list.append(4)
-my_linked_list.append(5)
+my_linked_list = None
+# my_linked_list.append(2)
+# my_linked_list.append(3)
+# my_linked_list.append(4)
+# my_linked_list.append(5)
+
+while True:
+    val = input("Enter a number: ")
+    if val == "stop":
+        break
+    if not val.isnumeric():
+        print("Error, not a number")
+        continue
+    if my_linked_list == None:
+        my_linked_list = LinkedList(val)
+        continue
+    my_linked_list.append(val)
 
 print( my_linked_list.find_middle_node().value )
 
--- LL_2.py
+++ LL_2.py
@@ -22,10 +22,16 @@ class LinkedList:
         return True
 
     # WRITE HAS_LOOP METHOD HERE #
-    #                            #
-    #                            #
-    #                            #
-    #                            #
+    def has_loop(self):
+        slow = self.head
+        fast = self.head.next
+        while True:
+            if fast == None or fast.next == None:
+                return False
+            if fast == slow:
+                return True
+            slow = slow.next
+            fast = fast.next.next
     ##############################
     
     
@@ -35,6 +41,7 @@ my_linked_list_1 = LinkedList(1)
 my_linked_list_1.append(2)
 my_linked_list_1.append(3)
 my_linked_list_1.append(4)
+my_linked_list_1.append(5)
 my_linked_list_1.tail.next = my_linked_list_1.head
 print(my_linked_list_1.has_loop() ) # Returns True
 
@@ -45,6 +52,7 @@ my_linked_list_2 = LinkedList(1)
 my_linked_list_2.append(2)
 my_linked_list_2.append(3)
 my_linked_list_2.append(4)
+my_linked_list_2.append(5)
 print(my_linked_list_2.has_loop() ) # Returns False
 
 
